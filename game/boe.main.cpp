abed63c415b9bb207f7f2d8d2980dec270ffa305
Sylae Jiendra Corell <sylae@calref.net>
2014-06-18 14:00:57 -0600

    Fixed 64-bit compiliation, edited configure script to allow setting of 64-bit operations

--- game/boe.main.cpp
+++ game/boe.main.cpp
@@ -18,24 +18,6 @@
 
 #include "globvar.h"
 
-// Check windows
-#if _WIN32 || _WIN64
-#if _WIN64
-#define ENVIRONMENT64
-#else
-#define ENVIRONMENT32
-#endif
-#endif
-
-// Check GCC
-#if __GNUC__
-#if __x86_64__ || __ppc64__
-#define ENVIRONMENT64
-#else
-#define ENVIRONMENT32
-#endif
-#endif
-
 void startup_load()
 {
 	load_file();
