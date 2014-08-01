1f566694ef4d0c142d68c0cb970014bcc11593ce
Sylae Jiendra Corell <sylae@calref.net>
2014-08-01 11:25:04 -0600

    Move declaration out of the header file

--- Win32/scenedit/dlogtool.cpp
+++ Win32/scenedit/dlogtool.cpp
@@ -149,6 +149,7 @@ short button_width[15] = {23,63,102,16,63, 63,63,63,6,14, 14,63,63,63,30};
 short button_height[15] = {23,23,23,13,23, 23,23,23,6,10,10,23,40,40,30};
 
 LRESULT CALLBACK dummy_dialog_proc (HWND hDlg, UINT message, WPARAM wparam, LPARAM lparam);
+LRESULT CALLBACK fresh_edit_proc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 FARPROC d_proc;
 
