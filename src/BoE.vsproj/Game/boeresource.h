233a50a1e24707b72e58b7bd93c868d7ee4e6a0c
Celtic Minstrel <celtic.minstrel.ca@gmail.com>
2015-06-09 10:26:08 -0400

    More fixes/tweaks to the installer - Add Windows icon for scenario files (using the old Mac icon) - Fix OpenAL not being deleted, causing the program directory to remain - Fix association for old scenarios (.exs) not being removed Also: - Fix build broken in previous commit (due to header renames)

--- src/BoE.vsproj/Game/boeresource.h
+++ src/BoE.vsproj/Game/boeresource.h
@@ -40,15 +40,16 @@
 #define IDM_HELP_TOWN                   143
 #define IDM_OPTIONS_JOURNAL             144
 #define IDI_BOESAVE                     145
+#define IDI_BOESCEN                     146
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        147
 #define _APS_NEXT_COMMAND_VALUE         32774
 #define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           146
+#define _APS_NEXT_SYMED_VALUE           147
 #endif
 #endif
