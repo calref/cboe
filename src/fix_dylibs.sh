4934bb220aeccab2273115b301526db5df4ebeed
Celtic Minstrel <celtic.minstrel.ca@some.place>
2015-06-24 14:44:18 -0400

    Fix not bundling Boost.Thread in XCode build

--- src/fix_dylibs.sh
+++ src/fix_dylibs.sh
@@ -11,11 +11,14 @@ echo Fixing boost dylib install names...
 EXEPATH=@executable_path/../Frameworks
 BOEPATH="$BUILT_PRODUCTS_DIR/$EXECUTABLE_PATH"
 BFSPATH="$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/libboost_filesystem.dylib"
+BTHPATH="$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/libboost_thread.dylib"
 
 # Update references in the executable file
 install_name_tool -change libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BOEPATH"
 install_name_tool -change libboost_filesystem.dylib "$EXEPATH/libboost_filesystem.dylib" "$BOEPATH"
+install_name_tool -change libboost_thread.dylib "$EXEPATH/libboost_thread.dylib" "$BOEPATH"
 
 # Update references within Boost
 install_name_tool -change libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BFSPATH"
+install_name_tool -change libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BTHPATH"
 
