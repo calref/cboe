e29e34582f50dd32431224ee99bf995534bea60a
Celtic Minstrel <celtic.minstrel.ca@gmail.com>
2015-08-19 13:13:36 -0400

    Get git revision inclusion working on Windows

--- src/tools/gitrev.sh
+++ src/tools/gitrev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-REVID=`/opt/local/bin/git rev-parse HEAD`;
-export FULLTAG=`/opt/local/bin/git tag --sort=v:refname | tail -n1`;
-TAGREV=`/opt/local/bin/git rev-parse $FULLTAG`;
+REVID=`git rev-parse HEAD`;
+export FULLTAG=`git tag --sort=v:refname | tail -n1`;
+TAGREV=`git rev-parse $FULLTAG`;
 
 echo -e "\n#define GIT_REVISION \"${REVID:0:7}\"\n#define GIT_TAG \"$FULLTAG\"\n#define GIT_TAG_REVISION \"${TAGREV:0:7}\"\n" > tools/gitrev.hpp
