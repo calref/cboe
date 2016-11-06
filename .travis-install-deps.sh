044543b71740db488984e2ba38805e74c26a7879
Celtic Minstrel <celtic.minstrel.ca@some.place>
2016-11-05 23:54:51 -0400

    Some fixups to Travis config

--- .travis-install-deps.sh
+++ .travis-install-deps.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -ev
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-	brew install scons sfml boost
+	# Also needs boost, but apparently that's already installed in Travis
+	brew install scons sfml
 else
 	sudo apt-get update -qq
 	sudo apt-get install -qq libsfml-dev libboost-dev
