570792420abe6c00ae0876fcaa4d261683d6a5dd
Celtic Minstrel <celtic.minstrel.ca@some.place>
2017-02-01 14:24:06 -0500

    Add two more animated portals
    One on grass and one on walkways

--- src/scenedit/scen.keydlgs.cpp
+++ src/scenedit/scen.keydlgs.cpp
@@ -168,7 +168,7 @@ pic_num_t choose_graphic(short cur_choice,ePicType g_type,cDialog* parent) {
 	cPictChoice* pic_dlg = nullptr;
 	switch(g_type + PIC_PRESET) {
 		case PIC_TER: total_pics = 680; break;
-		case PIC_TER_ANIM: total_pics = 28; break;
+		case PIC_TER_ANIM: total_pics = 30; break;
 		case PIC_DLOG: total_pics = 44; break;
 		case PIC_TALK: total_pics = 84; break;
 		case PIC_SCEN: total_pics = 30; break;
@@ -181,7 +181,7 @@ pic_num_t choose_graphic(short cur_choice,ePicType g_type,cDialog* parent) {
 		case PIC_MISSILE: total_pics = 16; break;
 		case PIC_STATUS: total_pics = 27; break;
 		case PIC_SCEN_LG: total_pics = 4; break;
-		case PIC_TER_MAP: total_pics = 988; break;
+		case PIC_TER_MAP: total_pics = 990; break;
 		case PIC_FULL:
 			if(!fs::is_directory(tempDir/scenario_temp_dir_name/"graphics")) {
 				showError("You have no custom graphics, so it's not possible to select this kind of picture!",parent);
