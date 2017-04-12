c9e265ca6285e6a9fb02ff80155f11810aa0de99
Celtic Minstrel <celtic.minstrel.ca@some.place>
2017-04-12 00:51:21 -0400

    Windows: Change several menu mnemonics and fix create town menuitem not working

--- rsrc/menus/scenresource.h
+++ rsrc/menus/scenresource.h
@@ -71,6 +71,7 @@
 #define IDM_FILE_SAVE_AS                166
 #define IDM_SCEN_RESIZE_OUTDOORS        167
 #define IDM_SCEN_ADV_IMPORT_OUT         168
+#define IDM_GRAYED_LABEL                169
 
 // Next default values for new objects
 // 
@@ -79,6 +80,6 @@
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40014
 #define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           169
+#define _APS_NEXT_SYMED_VALUE           170
 #endif
 #endif
