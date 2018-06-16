f9a901ea7f0b9b7e0f1e1dfd42dd89a0fb9e3875
Eric Ahnell <eric.ahnell@puttysoftware.com>
2018-06-16 15:44:48 -0400

    Fix dylibs properly.

--- pkg/mac/fix_dylibs.sh
+++ pkg/mac/fix_dylibs.sh
@@ -6,19 +6,28 @@
 #  Created by Celtic Minstrel on 14-04-17.
 #
 
-echo Fixing boost dylib install names...
+echo "Fixing boost dylib install names..."
 
-EXEPATH=@executable_path/../Frameworks
-BOEPATH="$BUILT_PRODUCTS_DIR/$EXECUTABLE_PATH"
-BFSPATH="$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/libboost_filesystem.dylib"
-BTHPATH="$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/libboost_thread.dylib"
+EXEPATH="Contents/Frameworks"
+BOEPATH="Contents/MacOS/$2"
+BFSPATH="$EXEPATH/libboost_filesystem.dylib"
+BTHPATH="$EXEPATH/libboost_thread-mt.dylib"
+
+# Fix permissions so we can change stuff.
+chmod +wx "$EXEPATH/libboost_system.dylib"
+chmod +wx "$EXEPATH/libboost_filesystem.dylib"
+chmod +wx "$EXEPATH/libboost_thread-mt.dylib"
 
 # Update references in the executable file
-install_name_tool -change libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BOEPATH"
-install_name_tool -change libboost_filesystem.dylib "$EXEPATH/libboost_filesystem.dylib" "$BOEPATH"
-install_name_tool -change libboost_thread.dylib "$EXEPATH/libboost_thread.dylib" "$BOEPATH"
+install_name_tool -rpath libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BOEPATH"
+install_name_tool -rpath libboost_filesystem.dylib "$EXEPATH/libboost_filesystem.dylib" "$BOEPATH"
+install_name_tool -rpath libboost_thread.dylib "$EXEPATH/libboost_thread-mt.dylib" "$BOEPATH"
 
 # Update references within Boost
-install_name_tool -change libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BFSPATH"
-install_name_tool -change libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BTHPATH"
+install_name_tool -rpath libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BFSPATH"
+install_name_tool -rpath libboost_system.dylib "$EXEPATH/libboost_system.dylib" "$BTHPATH"
 
+# Remove temporary write permissions.
+chmod -w "$EXEPATH/libboost_system.dylib"
+chmod -w "$EXEPATH/libboost_filesystem.dylib"
+chmod -w "$EXEPATH/libboost_thread-mt.dylib"
