fb4cf1bd7c76d9d939366f3b72908680caad7b37
Eric Ahnell <eric.ahnell@puttysoftware.com>
2018-06-16 21:57:17 -0400

    If we use rpaths, we need to tell install_name_tool to treat them as such.

--- pkg/mac/fix_dylibs.sh
+++ pkg/mac/fix_dylibs.sh
@@ -46,37 +46,23 @@ chmod +wx "$NEW_ABSOLUTE_SFML_GRAPHICS_PATH"
 chmod +wx "$NEW_ABSOLUTE_SFML_AUDIO_PATH"
 chmod +wx "$NEW_ABSOLUTE_SFML_WINDOW_PATH"
 
-# Add new paths
-install_name_tool -add_rpath "$NEW_BOOST_PATH" "$EXEPATH"
-install_name_tool -add_rpath "$NEW_BOOST_FS_PATH" "$EXEPATH"
-install_name_tool -add_rpath "$NEW_BOOST_THREAD_PATH" "$EXEPATH"
-install_name_tool -add_rpath "$NEW_SFML_PATH" "$EXEPATH"
-install_name_tool -add_rpath "$NEW_SFML_GRAPHICS_PATH" "$EXEPATH"
-install_name_tool -add_rpath "$NEW_SFML_AUDIO_PATH" "$EXEPATH"
-install_name_tool -add_rpath "$NEW_SFML_WINDOW_PATH" "$EXEPATH"
-install_name_tool -add_rpath "$NEW_BOOST_FS_PATH" "$NEW_ABSOLUTE_BOOST_PATH"
-install_name_tool -add_rpath "$NEW_BOOST_THREAD_PATH" "$NEW_ABSOLUTE_BOOST_PATH"
-install_name_tool -add_rpath "$NEW_SFML_GRAPHICS_PATH" "$NEW_ABSOLUTE_SFML_PATH"
-install_name_tool -add_rpath "$NEW_SFML_AUDIO_PATH" "$NEW_ABSOLUTE_SFML_PATH"
-install_name_tool -add_rpath "$NEW_SFML_WINDOW_PATH" "$NEW_ABSOLUTE_SFML_PATH"
-
 # Update references in the executable file
-install_name_tool -change "$OLD_BOOST_PATH" "$NEW_BOOST_PATH" "$EXEPATH"
-install_name_tool -change "$OLD_BOOST_FS_PATH" "$NEW_BOOST_FS_PATH" "$EXEPATH"
-install_name_tool -change "$OLD_BOOST_THREAD_PATH" "$NEW_BOOST_THREAD_PATH" "$EXEPATH"
-install_name_tool -change "$OLD_SFML_PATH" "$NEW_SFML_PATH" "$EXEPATH"
-install_name_tool -change "$OLD_SFML_GRAPHICS_PATH" "$NEW_SFML_GRAPHICS_PATH" "$EXEPATH"
-install_name_tool -change "$OLD_SFML_AUDIO_PATH" "$NEW_SFML_AUDIO_PATH" "$EXEPATH"
-install_name_tool -change "$OLD_SFML_WINDOW_PATH" "$NEW_SFML_WINDOW_PATH" "$EXEPATH"
+install_name_tool -rpath "$OLD_BOOST_PATH" "$NEW_BOOST_PATH" "$EXEPATH"
+install_name_tool -rpath "$OLD_BOOST_FS_PATH" "$NEW_BOOST_FS_PATH" "$EXEPATH"
+install_name_tool -rpath "$OLD_BOOST_THREAD_PATH" "$NEW_BOOST_THREAD_PATH" "$EXEPATH"
+install_name_tool -rpath "$OLD_SFML_PATH" "$NEW_SFML_PATH" "$EXEPATH"
+install_name_tool -rpath "$OLD_SFML_GRAPHICS_PATH" "$NEW_SFML_GRAPHICS_PATH" "$EXEPATH"
+install_name_tool -rpath "$OLD_SFML_AUDIO_PATH" "$NEW_SFML_AUDIO_PATH" "$EXEPATH"
+install_name_tool -rpath "$OLD_SFML_WINDOW_PATH" "$NEW_SFML_WINDOW_PATH" "$EXEPATH"
 
 # Update references within Boost
-install_name_tool -change "$OLD_BOOST_FS_PATH" "$NEW_BOOST_FS_PATH" "$NEW_ABSOLUTE_BOOST_PATH"
-install_name_tool -change "$OLD_BOOST_THREAD_PATH" "$NEW_BOOST_THREAD_PATH" "$NEW_ABSOLUTE_BOOST_PATH"
+install_name_tool -rpath "$OLD_BOOST_FS_PATH" "$NEW_BOOST_FS_PATH" "$NEW_ABSOLUTE_BOOST_PATH"
+install_name_tool -rpath "$OLD_BOOST_THREAD_PATH" "$NEW_BOOST_THREAD_PATH" "$NEW_ABSOLUTE_BOOST_PATH"
 
 # Update references within SFML
-install_name_tool -change "$OLD_SFML_GRAPHICS_PATH" "$NEW_SFML_GRAPHICS_PATH" "$NEW_ABSOLUTE_SFML_PATH"
-install_name_tool -change "$OLD_SFML_AUDIO_PATH" "$NEW_SFML_AUDIO_PATH" "$NEW_ABSOLUTE_SFML_PATH"
-install_name_tool -change "$OLD_SFML_WINDOW_PATH" "$NEW_SFML_WINDOW_PATH" "$NEW_ABSOLUTE_SFML_PATH"
+install_name_tool -rpath "$OLD_SFML_GRAPHICS_PATH" "$NEW_SFML_GRAPHICS_PATH" "$NEW_ABSOLUTE_SFML_PATH"
+install_name_tool -rpath "$OLD_SFML_AUDIO_PATH" "$NEW_SFML_AUDIO_PATH" "$NEW_ABSOLUTE_SFML_PATH"
+install_name_tool -rpath "$OLD_SFML_WINDOW_PATH" "$NEW_SFML_WINDOW_PATH" "$NEW_ABSOLUTE_SFML_PATH"
 
 # Remove temporary write permissions.
 chmod -w "$EXEPATH"
