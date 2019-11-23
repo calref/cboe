0001f754acd5e9f3bd82307348ec37589f32b504
Celtic Minstrel <CelticMinstrel@users.noreply.github.com>
2019-11-23 12:46:07 -0500

    Travis: Don't auto-update Homebrew on OSX builds

--- .travis-install-deps.sh
+++ .travis-install-deps.sh
@@ -4,7 +4,7 @@ set -ev
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	# Also needs boost, but apparently that's already installed in Travis
-	brew install scons sfml
+	HOMEBREW_NO_AUTO_UPDATE=1 brew install scons sfml
 else
 	sudo apt-get update -qq
 	sudo apt-get install -qq libsfml-dev libboost-dev libboost-filesystem-dev libboost-thread-dev libxml2-utils
