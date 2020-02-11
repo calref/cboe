4711871a1c94e1a574af02b52d268014ede6f244
xq <the@interwebs.test>
2020-02-11 04:24:17 +0200

    Adjust damage_pc to report damage values as well

--- src/game/boe.combat.cpp
+++ src/game/boe.combat.cpp
@@ -2850,7 +2850,7 @@ void monster_attack(short who_att,iLiving* target) {
 				store_hp = target->get_health();
 				sound_type = get_monst_sound(attacker,i);
 				size_t i_monst = univ.get_target_i(*target);
-				bool damaged = false;
+				short damaged = false;
 				if(m_target != nullptr) {
 					// TODO: Maybe this damage should be printed?
 					damaged = damage_monst(*m_target,7,r2,dam_type,sound_type,false);
