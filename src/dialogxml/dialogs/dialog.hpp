4b016f7a3459967bd3a42f9193a51522a36fce01
Laurent Alonso(fr) <alonso.laurent@gmail.fr>
2020-06-10 18:45:26 +0200

    UI scaling: try to also scale the dialogs...

--- src/dialogxml/dialogs/dialog.hpp
+++ src/dialogxml/dialogs/dialog.hpp
@@ -38,6 +38,7 @@ enum eLabelPos {
 class cDialog {
 	typedef std::map<std::string,cControl*>::iterator ctrlIter;
 	std::map<std::string,cControl*> controls;
+	float ui_scale;
 	short bg;
 	sf::Color defTextClr;
 	sf::RenderWindow win;
@@ -57,11 +58,11 @@ public:
 	static short defaultBackground;
 	/// Create a new dialog with no items.
 	/// @param p Optionally, a parent dialog.
-	explicit cDialog(cDialog* p = nullptr);
+	explicit cDialog(cDialog* p = nullptr, float scaling = -1);
 	/// Creates a new dialog, loading its definition from a file.
 	/// @param path The name of the file to load. It must be in the game's dialogs directory.
 	/// @param p Optionally, a parent dialog.
-	explicit cDialog(std::string path, cDialog* p = nullptr); // cd_create_dialog
+	explicit cDialog(std::string path, cDialog* p = nullptr, float scaling = -1); // cd_create_dialog
 	~cDialog(); // cd_kill_dialog
 	/// Add a new control to the dialog.
 	/// @param what A pointer to the control, which should already be constructed.
