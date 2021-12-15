calref/cboe

commit 6069fc7716580e787c27715627b6eb6b5bb3d0dc

Try to clean a little the code by merging the functions which display pc, monster, ...   to be continued...

src/scenario/special.cpp
@@ -238,50 +238,20 @@ void cSpecial::import_legacy(legacy::special_node_type const &old){
 		case 85: type = eSpecType::AFFECT_SKILL_PTS; break;
 		case 86: type = eSpecType::AFFECT_DEADNESS; break;
 			// Affect status effect (eleven individual node types were collapsed into one)
-		case 87:
+		case 87: case 88:  case 89:
+		case 90: case 91:  case 92:
+		case 93: case 94:  case 95:
+		case 96: case 97: {
+			eStatus const status[]= {
+				eStatus::POISON, eStatus::HASTE_SLOW, eStatus::INVULNERABLE,
+				eStatus::MAGIC_RESISTANCE, eStatus::WEBS, eStatus::DISEASE,
+				eStatus::INVISIBLE, eStatus::BLESS_CURSE, eStatus::DUMB,
+				eStatus::ASLEEP, eStatus::PARALYZED
+			};
 			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::POISON);
+			ex1c = int(status[old.type-87]);
 			break;
-		case 88:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::HASTE_SLOW);
-			break;
-		case 89:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::INVULNERABLE);
-			break;
-		case 90:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::MAGIC_RESISTANCE);
-			break;
-		case 91:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::WEBS);
-			break;
-		case 92:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::DISEASE);
-			break;
-		case 93:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::INVISIBLE);
-			break;
-		case 94:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::BLESS_CURSE);
-			break;
-		case 95:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::DUMB);
-			break;
-		case 96:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::ASLEEP);
-			break;
-		case 97:
-			type = eSpecType::AFFECT_STATUS;
-			ex1c = int(eStatus::PARALYZED);
-			break;
+		}
 		case 98: type = eSpecType::AFFECT_STAT; break;
 		case 99: case 100: // Add mage/priest spell TODO: Merge these by adding 100 if it's a priest spell
 			if(old.type == 99) type = eSpecType::AFFECT_MAGE_SPELL;
@@ -293,22 +263,14 @@ void cSpecial::import_legacy(legacy::special_node_type const &old){
 		case 102: type = eSpecType::AFFECT_FOOD; break;
 		case 103: type = eSpecType::AFFECT_ALCHEMY; break;
 			// Party statuses (three nodes collapsed into one)
-		case 104:
+		case 104: case 105: case 106: {
+			ePartyStatus const status[]={ ePartyStatus::STEALTH, ePartyStatus::FIREWALK, ePartyStatus::FLIGHT};
 			type = eSpecType::AFFECT_PARTY_STATUS;
 			ex1b = 0;
-			ex2a = int(ePartyStatus::STEALTH);
+			ex2a = int(status[old.type-104]);
 			break;
-		case 105:
-			type = eSpecType::AFFECT_PARTY_STATUS;
-			ex1b = 0;
-			ex2a = int(ePartyStatus::FIREWALK);
-			break;
-		case 106:
-			type = eSpecType::AFFECT_PARTY_STATUS;
-			ex1b = 0;
-			ex2a = int(ePartyStatus::FLIGHT);
-			break;
-			
+		}
+		
 			// 107-129 were undefined
 			
 		case 130: type = eSpecType::IF_SDF; break;
@@ -320,25 +282,18 @@ void cSpecial::import_legacy(legacy::special_node_type const &old){
 			type = eSpecType::IF_TER_TYPE;
 			break;
 		case 137: case 142: // If has gold (and maybe take)
-			type = eSpecType::IF_HAS_GOLD;
-			ex2a = (old.type - 137) / 5;
-			break;
 		case 138: case 143: // If has food (and maybe take)
-			type = eSpecType::IF_HAS_FOOD;
-			ex2a = (old.type - 138) / 5;
-			break;
 		case 139: case 144: // If item on space (and maybe take)
-			type = eSpecType::IF_ITEM_CLASS_ON_SPACE;
-			ex2c = (old.type - 139) / 5;
-			break;
 		case 140: case 145: // If have item class (and maybe take)
-			type = eSpecType::IF_HAVE_ITEM_CLASS;
-			ex2a = (old.type - 140) / 5;
+		case 141: case 146: { // If equip item class (and maybe take)
+			eSpecType const types[]={
+				eSpecType::IF_HAS_GOLD, eSpecType::IF_HAS_FOOD, eSpecType::IF_ITEM_CLASS_ON_SPACE,
+				eSpecType::IF_HAVE_ITEM_CLASS, eSpecType::IF_EQUIP_ITEM_CLASS
+			};
+			type = types[(old.type-137)%5];
+			ex2a = (old.type<142 ? 0 : 1);
 			break;
-		case 141: case 146: // If equip item class (and maybe take)
-			type = eSpecType::IF_EQUIP_ITEM_CLASS;
-			ex2a = (old.type - 141) / 5;
-			break;
+		}
 		case 147: type = eSpecType::IF_DAY_REACHED; break;
 		case 148: case 149: // if barrels or crates
 			type = eSpecType::IF_FIELDS;
@@ -385,16 +340,10 @@ void cSpecial::import_legacy(legacy::special_node_type const &old){
 
 			// 157-169 were undefined
 			
-		case 170: type = eSpecType::MAKE_TOWN_HOSTILE; ex2a=1; break; // OSNOLA: make town always hostile
-		case 171: // Change terrain (town/outdoor)
-			type = eSpecType::CHANGE_TER;
-			break;
-		case 172: // Swap terrain
-			type = eSpecType::SWAP_TER;
-			break;
-		case 173: // Transform terrain
-			type = eSpecType::TRANS_TER;
-			break;
+		case 170: type = eSpecType::MAKE_TOWN_HOSTILE; ex2a=1; break;
+		case 171: type = eSpecType::CHANGE_TER; break; // Change town terrain
+		case 172: type = eSpecType::SWAP_TER; break;
+		case 173: type = eSpecType::TRANS_TER; break;
 		case 174: type = eSpecType::TOWN_MOVE_PARTY; break;
 		case 175: type = eSpecType::TOWN_HIT_SPACE; break;
 		case 176: type = eSpecType::TOWN_EXPLODE_SPACE; break;
@@ -441,50 +390,20 @@ void cSpecial::import_legacy(legacy::special_node_type const &old){
 			// 198-199 were undefined
 			
 			// Place fields (twelve individual node types were collapsed into one)
-		case 200:
+		case 200: case 201: case 202:
+		case 203: case 204: case 205:
+		case 206: case 207: case 208:
+		case 209: case 210: {
+			eFieldType const fields[]={
+				WALL_FIRE, WALL_FORCE, WALL_ICE,
+				WALL_BLADES, CLOUD_STINK, CLOUD_SLEEP,
+				FIELD_QUICKFIRE, BARRIER_FIRE, BARRIER_FORCE,
+				FIELD_DISPEL, SFX_SMALL_BLOOD
+			};
 			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = WALL_FIRE;
+			sd2 = fields[old.type-200];
 			break;
-		case 201:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = WALL_FORCE;
-			break;
-		case 202:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = WALL_ICE;
-			break;
-		case 203:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = WALL_BLADES;
-			break;
-		case 204:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = CLOUD_STINK;
-			break;
-		case 205:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = CLOUD_SLEEP;
-			break;
-		case 206:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = FIELD_QUICKFIRE;
-			break;
-		case 207:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = BARRIER_FIRE;
-			break;
-		case 208:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = BARRIER_FORCE;
-			break;
-		case 209:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 = FIELD_DISPEL;
-			break;
-		case 210:
-			type = eSpecType::RECT_PLACE_FIELD;
-			sd2 += SFX_SMALL_BLOOD;
-			break;
+		}
 		case 211:
 			type = eSpecType::RECT_PLACE_FIELD;
 			switch(old.sd2) {
@@ -504,9 +423,7 @@ void cSpecial::import_legacy(legacy::special_node_type const &old){
 			// 219-224 were undefined
 			
 		case 225: type = eSpecType::OUT_MAKE_WANDER; break;
-		case 226: // Change outdoor terrain
-			type = eSpecType::CHANGE_TER;
-			break;
+		case 226: type = eSpecType::CHANGE_TER; break; // Change outdoor terrain
 		case 227: type = eSpecType::OUT_PLACE_ENCOUNTER; break;
 		case 228: type = eSpecType::OUT_MOVE_PARTY; break;
 		case 229: // Outdoor store - fix spell IDs
@@ -537,10 +454,8 @@ void cSpecial::import_legacy(legacy::special_node_type const &old){
 			else type = eSpecType::TOWN_STAIR; // 190
 			if(type != eSpecType::ONCE_DIALOG) break;
 			// Duplicate Leave button
-			if(old.ex1a == 20)
-				ex1a = 9;
-			if(old.ex2a == 20)
-				ex2a = 9;
+			if(old.ex1a == 20) ex1a = 9;
+			if(old.ex2a == 20) ex2a = 9;
 			break;
 		}
 			
@@ -694,7 +609,7 @@ static const char*const button_dict[7][11] = {
 	}
 };
 
-static int offsets[] = {
+static int const offsets[] = {
 	int(eSpecType::NONE),
 	int(eSpecType::ONCE_GIVE_ITEM),
 	int(eSpecType::SELECT_TARGET),

src/dialogxml/widgets/pict.cpp
@@ -51,6 +51,7 @@ void cPict::init(){
 	drawPict()[PIC_CUSTOM_TALK] = &cPict::drawCustomTalk;
 	drawPict()[PIC_CUSTOM_SCEN] = &cPict::drawCustomTalk;
 	drawPict()[PIC_CUSTOM_ITEM] = &cPict::drawCustomItem;
+	drawPict()[PIC_CUSTOM_PC] = &cPict::drawCustomMonstSm; // checkme, I am not sure that this can happen
 	drawPict()[PIC_CUSTOM_TINY_ITEM] = &cPict::drawCustomTinyItem;
 	drawPict()[PIC_CUSTOM_FULL] = &cPict::drawFullSheet;
 	drawPict()[PIC_CUSTOM_BOOM] = &cPict::drawCustomBoom;
@@ -100,7 +101,7 @@ void cPict::setPict(pic_num_t num, ePicType type, bool updateResultType){
 		resultType = type;
 	else
 		fromType = type;
-	if(type == PIC_MONST && resultType == PIC_MONST && picNum < 1000) {
+	if(type == PIC_MONST && resultType == PIC_MONST && num < m_pic_index.size()) {
 		if(m_pic_index[num].x == 2) resultType += PIC_WIDE;
 		if(m_pic_index[num].y == 2) resultType += PIC_TALL;
 	}
@@ -779,6 +780,20 @@ try {
 			source = *ResMgr::textures.get("monst"+std::to_string((pictId/20)+1));
 			break;
 		}
+		case ePicType::PIC_PC: {
+			if (pict.num>=100)
+				throw "bad index";
+			from_rect = calc_rect(2*(pict.num/8)+((anim&2) ? 1 : 0), (pict.num%8) + ((anim&1) ? 8 : 0));
+			source = *ResMgr::textures.get("pcs");
+			break;
+		}
+		case ePicType::PIC_CUSTOM_PC:
+			std::tie(source,from_rect) = spec_scen_g.find_graphic(pict.num+(anim&2) ? 1 : 0, false);
+			break;
+		case ePicType::PIC_PARTY_PC:
+			std::tie(source,from_rect) = spec_scen_g.find_graphic(pict.num+(anim&2) ? 1 : 0, true);
+			break;
+
 		// dialog
 		case ePicType::PIC_DLOG:
 			from_rect = {0,0,36,36};
@@ -857,15 +872,14 @@ void cPict::drawPresetTerAnim(short num, rectangle to_rect){
 }
 
 void cPict::drawPresetMonstSm(short num, rectangle to_rect){
-	short m_start_pic = m_pic_index[num].i;
-	auto from_gw = getSheet(SHEET_MONST, m_start_pic / 20);
-	if(!from_gw) return;
-	m_start_pic = m_start_pic % 20;
-	rectangle from_rect = calcDefMonstRect(m_start_pic, animFrame);
+	Texture source;
+	rectangle source_rect;
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_MONST)), source, source_rect, animFrame))
+		return;
 	to_rect.right = to_rect.left + 28;
 	to_rect.bottom = to_rect.top + 36;
 	fill_rect(*inWindow, to_rect, sf::Color::Black);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, to_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, to_rect, sf::BlendAlpha);
 }
 
 void cPict::drawPresetMonstWide(short num, rectangle to_rect){
@@ -873,19 +887,17 @@ void cPict::drawPresetMonstWide(short num, rectangle to_rect){
 	to_rect.right = to_rect.left + 28; to_rect.bottom = to_rect.top + 36;
 	fill_rect(*inWindow, to_rect, sf::Color::Black);
 	
-	short m_start_pic = m_pic_index[num].i;
-	auto from_gw = getSheet(SHEET_MONST, m_start_pic / 20);
-	if(!from_gw) return;
-	rectangle from_rect = calcDefMonstRect(m_start_pic % 20, animFrame);
+	Texture source;
+	rectangle source_rect;
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_MONST)), source, source_rect, animFrame))
+		return;
 	small_monst_rect.offset(to_rect.left,to_rect.top + 7);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
 	
-	m_start_pic = m_pic_index[num].i + 1;
-	from_gw = getSheet(SHEET_MONST, m_start_pic / 20);
-	if(!from_gw) return;
-	from_rect = calcDefMonstRect(m_start_pic % 20, animFrame);
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_MONST)), source, source_rect, animFrame, 1))
+		return;
 	small_monst_rect.offset(14,0);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
 }
 
 void cPict::drawPresetMonstTall(short num, rectangle to_rect){
@@ -894,19 +906,17 @@ void cPict::drawPresetMonstTall(short num, rectangle to_rect){
 	to_rect.bottom = to_rect.top + 36;
 	fill_rect(*inWindow, to_rect, sf::Color::Black);
 	
-	short m_start_pic = m_pic_index[num].i;
-	auto from_gw = getSheet(SHEET_MONST, m_start_pic / 20);
-	if(!from_gw) return;
-	rectangle from_rect = calcDefMonstRect(m_start_pic % 20, animFrame);
+	Texture source;
+	rectangle source_rect;
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_MONST)), source, source_rect, animFrame))
+		return;
 	small_monst_rect.offset(to_rect.left + 7,to_rect.top);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
 	
-	m_start_pic = m_pic_index[num].i + 1;
-	from_gw = getSheet(SHEET_MONST, m_start_pic / 20);
-	if(!from_gw) return;
-	from_rect = calcDefMonstRect(m_start_pic % 20, animFrame);
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_MONST)), source, source_rect, animFrame, 1))
+		return;
 	small_monst_rect.offset(0,18);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
 }
 
 void cPict::drawPresetMonstLg(short num, rectangle to_rect){
@@ -915,33 +925,27 @@ void cPict::drawPresetMonstLg(short num, rectangle to_rect){
 	to_rect.bottom = to_rect.top + 36;
 	fill_rect(*inWindow, to_rect, sf::Color::Black);
 	
-	short m_start_pic = m_pic_index[num].i;
-	auto from_gw = getSheet(SHEET_MONST, m_start_pic / 20);
-	if(!from_gw) return;
-	rectangle from_rect = calcDefMonstRect(m_start_pic % 20, animFrame);
+	Texture source;
+	rectangle source_rect;
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_MONST)), source, source_rect, animFrame))
+		return;
 	small_monst_rect.offset(to_rect.left,to_rect.top);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
 	
-	m_start_pic = m_pic_index[num].i + 1;
-	from_gw = getSheet(SHEET_MONST, m_start_pic / 20);
-	if(!from_gw) return;
-	from_rect = calcDefMonstRect(m_start_pic % 20, animFrame);
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_MONST)), source, source_rect, animFrame, 1))
+		return;
 	small_monst_rect.offset(14,0);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
 	
-	m_start_pic = m_pic_index[num].i + 2;
-	from_gw = getSheet(SHEET_MONST, m_start_pic / 20);
-	if(!from_gw) return;
-	from_rect = calcDefMonstRect(m_start_pic % 20, animFrame);
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_MONST)), source, source_rect, animFrame, 2))
+		return;
 	small_monst_rect.offset(-14,18);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
 	
-	m_start_pic = m_pic_index[num].i + 3;
-	from_gw = getSheet(SHEET_MONST, m_start_pic / 20);
-	if(!from_gw) return;
-	from_rect = calcDefMonstRect(m_start_pic % 20, animFrame);
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_MONST)), source, source_rect, animFrame, 3))
+		return;
 	small_monst_rect.offset(14,0);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, small_monst_rect, sf::BlendAlpha);
 }
 
 void cPict::drawPresetDlog(short num, rectangle to_rect){
@@ -1008,7 +1012,7 @@ void cPict::drawPresetItem(short num, rectangle to_rect){
 	auto sourceType=getSourceType(ePicType::PIC_ITEM);
 	if (!get_picture(cPictNum(num, sourceType), source, source_rect, animFrame))
 		return;
-	if (sourceType==ePicType::PIC_ITEM && num>=55)
+	if (sourceType==ePicType::PIC_TINY_ITEM || (sourceType==ePicType::PIC_ITEM && num>=55))
 		to_rect.inset(5,9);
 	rect_draw_some_item(source, source_rect, *inWindow, to_rect, sf::BlendAlpha);
 }
@@ -1026,12 +1030,14 @@ void cPict::drawPresetTinyItem(short num, rectangle to_rect){
 }
 
 void cPict::drawPresetPc(short num, rectangle to_rect){
-	auto from_gw = getSheet(SHEET_PC);
-	rectangle from_rect = calc_rect(2 * (num / 8), num % 8);
+	Texture source;
+	rectangle source_rect;
+	if (!get_picture(cPictNum(num, getSourceType(ePicType::PIC_PC)), source, source_rect, animFrame))
+		return;
 	to_rect.right = to_rect.left + 28;
 	to_rect.bottom = to_rect.top + 36;
 	fill_rect(*inWindow, to_rect, sf::Color::Black);
-	rect_draw_some_item(*from_gw, from_rect, *inWindow, to_rect, sf::BlendAlpha);
+	rect_draw_some_item(source, source_rect, *inWindow, to_rect, sf::BlendAlpha);
 }
 
 void cPict::drawPresetField(short num, rectangle to_rect){

src/dialogxml/widgets/pictypes.hpp
@@ -43,6 +43,7 @@ enum ePicType {
 	PIC_CUSTOM_TALK = 105,		///< 32x32 talking portrait drawn from two 16x32 halves in the custom sheets
 	PIC_CUSTOM_SCEN = 106,		///< 32x32 scenario portrait loading from scenname.exr/scenario.png
 	PIC_CUSTOM_ITEM = 107,		///< 28x36 custom item graphic from the custom sheets
+	PIC_CUSTOM_PC = 108,		///< 28x36 custom pc graphic from the custom sheets
 	PIC_CUSTOM_BOOM = 110,		///< 28x36 custom explosion graphic (8 frames)
 	PIC_CUSTOM_FULL = 111,		///< entire sheet graphic, drawn from scenname.exr/sheetxxx.png where xxx is the number
 	PIC_CUSTOM_MISSILE = 112,	///< 18x18 missile graphic drawn from the the custom sheets

src/game/boe.dlgutil.cpp
@@ -1342,7 +1342,7 @@ static void put_party_stats(cDialog& me) {
 			me["delete" + n].setText("Delete");
 			me["pic" + n].show();
 			me["pc" + n].show();
-			dynamic_cast<cPict&>(me["pc" + n]).setPict(univ.party[i].which_graphic);
+			dynamic_cast<cPict&>(me["pc" + n]).setPict(univ.party[i].get_picture_num());
 		}
 		else {
 			me["name" + n].setText("Empty.");

src/game/boe.graphics.cpp
@@ -312,27 +312,13 @@ void draw_startup_stats() {
 			if(univ.party[i].main_status != eMainStatus::ABSENT) {
 				to_rect = party_to;
 				to_rect.offset(pc_rect.left,pc_rect.top);
-				pic_num_t pic = univ.party[i].which_graphic;
 				Texture gw;
-				if(pic >= 1000) {
-					std::tie(gw,from_rect) = spec_scen_g.find_graphic(pic % 1000, pic >= 10000);
+				if (cPict::get_picture(univ.party[i].get_picture_num(), gw, from_rect))
 					rect_draw_some_item(gw,from_rect,mainPtr,to_rect,sf::BlendAlpha);
-				} else if(pic >= 100) {
-					// Note that we assume it's a 1x1 graphic.
-					// PCs can't be larger than that, but we leave it to the scenario designer to avoid assigning larger graphics.
-					if (cPict::get_picture(cPictNum(pic-100,PIC_MONST), gw, from_rect))
-						rect_draw_some_item(gw,from_rect,mainPtr,to_rect,sf::BlendAlpha);
-				} else {
-					from_rect = calc_rect(2 * (pic / 8), pic % 8);
-					auto const & pc_gworld = *ResMgr::textures.get("pcs");
-					rect_draw_some_item(pc_gworld,from_rect,mainPtr,to_rect,sf::BlendAlpha);
-				}
-				
 				style.pointSize = 14;
 				pc_rect.offset(35,0);
 				win_draw_string(mainPtr,pc_rect,univ.party[i].name,eTextMode::WRAP,style);
-				to_rect.offset(pc_rect.left + 8,pc_rect.top + 8);
-				
+				to_rect.offset(pc_rect.left + 8,pc_rect.top + 8);				
 			}
 			style.pointSize = 12;
 			pc_rect.offset(12,16);
@@ -865,8 +851,7 @@ void draw_terrain(short	mode) {
 			// TODO: Move draw_sfx, draw_items, draw_fields, draw_spec_items, etc to here
 			
 			if(is_town() || is_combat())
-				draw_items(where_draw);
-			// ASAN: where_draw.y can be equal to -1, ...
+				draw_items(where_draw); // todo, create the list of item to draw outside this loop
 			int max_dim = is_town() ? univ.town->max_dim : 96;
 			if (where_draw.x<0 || where_draw.x>=max_dim || where_draw.y<0 || where_draw.y>=max_dim) {
 				place_road(q,r,where_draw,false);

src/game/boe.graphutil.cpp
@@ -181,30 +181,8 @@ void draw_combat_pc(cPlayer& who, location center, bool attacking) {
 			location where_draw(who.combat_pos.x - center.x + 4, who.combat_pos.y - center.y + 4);
 			rectangle source_rect;
 			Texture from_gw;
-			pic_num_t pic = who.which_graphic;
-			if(pic >= 1000) {
-				bool isParty = pic >= 10000;
-				pic_num_t need_pic = pic % 1000;
-				if(who.direction >= 4)
-					need_pic++;
-				if(attacking)
-					need_pic += 2;
-				std::tie(from_gw,source_rect) = spec_scen_g.find_graphic(need_pic, isParty);
-			} else if(pic >= 100) {
-				// Note that we assume it's a 1x1 graphic.
-				// PCs can't be larger than that, but we leave it to the scenario designer to avoid assigning larger graphics.
-				cPict::get_picture(cPictNum(pic - 100,PIC_MONST), from_gw, source_rect, (who.direction < 4 ? 0 : 2) + (attacking ? 1 : 0));
-			} else {
-				source_rect = calc_rect(2 * (pic / 8), pic % 8);
-				if(who.direction >= 4)
-					source_rect.offset(28,0);
-				if(attacking)
-					source_rect.offset(0,288);
-				from_gw = *ResMgr::textures.get("pcs");
-			}
-			
-			Draw_Some_Item(from_gw, source_rect, terrain_screen_gworld, where_draw, 1, 0);
-			
+			if (cPict::get_picture(who.get_picture_num(), from_gw, source_rect, (who.direction >= 4 ? 2 : 0) + (attacking ? 1 : 0)))
+				Draw_Some_Item(from_gw, source_rect, terrain_screen_gworld, where_draw, 1, 0);
 		}
 }
 
@@ -409,24 +387,6 @@ void draw_party_symbol(location center) {
 	
 	if((univ.party.in_boat < 0) && (univ.party.in_horse < 0)) {
 		i = first_active_pc();
-		Texture from_gw;
-		pic_num_t pic = univ.party[i].which_graphic;
-		if(pic >= 1000) {
-			bool isParty = pic >= 10000;
-			pic_num_t need_pic = pic % 1000;
-			if(univ.party.direction >= 4)
-				need_pic++;
-			std::tie(from_gw,source_rect) = spec_scen_g.find_graphic(need_pic, isParty);
-		} else if(pic >= 100) {
-			// Note that we assume it's a 1x1 graphic.
-			// PCs can't be larger than that, but we leave it to the scenario designer to avoid assigning larger graphics.
-			cPict::get_picture(cPictNum(pic - 100,PIC_MONST), from_gw, source_rect, univ.party.direction < 4 ? 0 : 2);
-		} else {
-			source_rect = calc_rect(2 * (pic / 8), pic % 8);
-			if(univ.party.direction >= 4)
-				source_rect.offset(28,0);
-			from_gw = *ResMgr::textures.get("pcs");
-		}
 		ter_num_t ter = 0;
 		if(is_out())
 			ter = univ.out[univ.party.out_loc.x][univ.party.out_loc.y];
@@ -435,7 +395,11 @@ void draw_party_symbol(location center) {
 		// now wedge in bed graphic
 		if(is_town() && univ.scenario.get_terrain(ter).special == eTerSpec::BED)
 			draw_one_terrain_spot((short) target.x,(short) target.y,10000 + univ.get_terrain(ter).flag1);
-		else Draw_Some_Item(from_gw, source_rect, terrain_screen_gworld, target, 1, 0);
+		else {
+			Texture from_gw;
+			cPict::get_picture(univ.party[i].get_picture_num(), from_gw, source_rect, (univ.party.direction >= 4 ? 2 : 0));
+			Draw_Some_Item(from_gw, source_rect, terrain_screen_gworld, target, 1, 0);
+		}
 	}
 	else if(univ.party.in_boat >= 0) {
 		if(univ.party.direction == DIR_N) i = 2;

src/game/boe.graphutil.hpp
@@ -15,7 +15,6 @@ void draw_pcs(location center);
 void draw_outd_boats(location center);
 void draw_town_boat(location center) ;
 void draw_party_symbol(location center);
-rectangle get_monster_template_rect (pic_num_t type_wanted,short mode,short which_part) ;
 bool is_fluid(ter_num_t ter_type);
 bool is_shore(ter_num_t ter_type);
 bool is_wall(ter_num_t ter_type);

src/game/boe.infodlg.cpp
@@ -353,10 +353,7 @@ static void display_pc_info(cDialog& me, const short pc_num) {
 	me["xp"].setTextToNum(pc.experience);
 	me["skp"].setTextToNum(pc.skill_pts);
 	me["progress"].setTextToNum(pc.level * pc.get_tnl());
-	pic_num_t pic = pc.which_graphic;
-	if(pic >= 100 && pic < 1000)
-		dynamic_cast<cPict&>(me["pic"]).setPict(pic - 100,PIC_MONST);
-	else dynamic_cast<cPict&>(me["pic"]).setPict(pic,PIC_PC);
+	dynamic_cast<cPict&>(me["pic"]).setPict(pc.get_picture_num());
 	
 	// Fight bonuses
 	auto weapons = pc.get_weapons();

commit d7568592743c1f741aa2e6767da69494bf07a8db

special[check item on place]:correction of a bug introduced in 6069fc7716580e787c27715627b6eb6b5bb3d0dc

src/scenario/special.cpp
@@ -270,9 +270,8 @@ void cSpecial::import_legacy(legacy::special_node_type const &old){
 			ex2a = int(status[old.type-104]);
 			break;
 		}
-		
+
 			// 107-129 were undefined
-			
 		case 130: type = eSpecType::IF_SDF; break;
 		case 131: type = eSpecType::IF_TOWN_NUM; break;
 		case 132: type = eSpecType::IF_RANDOM; break;
@@ -291,7 +290,10 @@ void cSpecial::import_legacy(legacy::special_node_type const &old){
 				eSpecType::IF_HAVE_ITEM_CLASS, eSpecType::IF_EQUIP_ITEM_CLASS
 			};
 			type = types[(old.type-137)%5];
-			ex2a = (old.type<142 ? 0 : 1);
+			if (old.type==139 || old.type==144) // ex1a, ex2a is used to store the item'slocation
+				ex2c = (old.type<142 ? 0 : 1);
+			else
+				ex2a = (old.type<142 ? 0 : 1);
 			break;
 		}
 		case 147: type = eSpecType::IF_DAY_REACHED; break;