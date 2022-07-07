4783c247798fa5077fb1a9eab856bf6c335b1d50
Celtic Minstrel <celtic.minstrel.ca@some.place>
2022-07-07 09:19:51 -0400

    Add CI status badge to the readme, plus a missing dependency

--- .github/workflows/scripts/linux/install-tgui.sh
+++ .github/workflows/scripts/linux/install-tgui.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ve
 
-git clone --depth 1 -b 0.8 https://github.com/texus/TGUI.git
+git clone --depth 1 -b 0.9 https://github.com/texus/TGUI.git
 cd TGUI
 export CLICOLOR_FORCE=1
 cmake .
