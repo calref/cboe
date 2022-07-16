441ebb358f9312be01cbbaa286167d5bc69adf7d
ALONSO Laurent <laurent.alonso@inria.fr>
2022-07-16 13:19:28 +0200

    legacy[save]: try to solve many problem + force monst.which_town to be set ( to fix a problem when saving context)

--- src/porting.hpp
+++ src/porting.hpp
@@ -15,11 +15,14 @@ void flip_spec_node(legacy::special_node_type *spec);
 void flip_short(int16_t *s);
 void flip_rect(legacy::Rect* s);
 
+void port_t_i(legacy::town_item_list* old);
 void port_t_d(legacy::big_tr_type* old);
 void port_ave_t(legacy::ave_tr_type* old);
 void port_tiny_t(legacy::tiny_tr_type* old);
 void port_scenario(legacy::scenario_data_type* old);
+void port_item_record(legacy::item_record_type *old);
 void port_item_list(legacy::scen_item_data_type* old);
+void port_stored_items_list(legacy::stored_items_list_type *old);
 void port_out(legacy::outdoor_record_type* out);
 void port_talk_nodes(legacy::talking_record_type* dummy_talk_ptr);
 void port_town(legacy::town_record_type* dummy_town_ptr);
