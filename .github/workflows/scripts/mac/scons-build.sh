57a6901dab54b274e86f929fdc3b245e854edafb
Celtic Minstrel <celtic.minstrel.ca@some.place>
2023-01-21 15:48:56 -0500

    CI: Enable the mac-scons build

--- .github/workflows/scripts/mac/scons-build.sh
+++ .github/workflows/scripts/mac/scons-build.sh
@@ -2,5 +2,6 @@
 
 export CC="$(brew --prefix llvm)/bin/clang"
 export CXX="$(brew --prefix llvm)/bin/clang++"
+export SDKROOT="$(xcrun --show-sdk-path)"
 
 scons CXXFLAGS="-I/usr/local/opt/zlib/include" LINKFLAGS="-L/usr/local/opt/zlib/lib"
