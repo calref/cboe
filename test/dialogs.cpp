effc355fe10880d995033816f23d32e20eb04f95
Celtic Minstrel <celtic.minstrel.ca@some.place>
2023-01-21 16:36:31 -0500

    Enable the dialogs test in Xcode and fix one dialog that failed
    This also makes the test itself better

--- test/dialogs.cpp
+++ test/dialogs.cpp
@@ -8,23 +8,35 @@
 #include "catch.hpp"
 #include <boost/filesystem.hpp>
 #include "fileio/resmgr/res_dialog.hpp"
+#include "fileio/resmgr/res_font.hpp"
+#include "fileio/resmgr/res_image.hpp"
 #include "dialogxml/dialogs/dialog.hpp"
 
-TEST_CASE("Construct each type of dialog in the engine") {
-	fs::path dialogsPath = fs::current_path()/".."/"Blades of Exile"/"data"/"dialogs";
-	fs::directory_iterator it{dialogsPath};
-	auto end = fs::directory_iterator{};
+static void load_dialog(fs::path path) {
+	DialogDefn* defn = nullptr;
+	try {
+		defn = load_dialog_defn(path);
+		cDialog dialog(*defn);
+	} catch(...) {
+		if(defn != nullptr) delete defn;
+		throw;
+	}
+}
+
+TEST_CASE("Construct each type of dialog in the engine") try {
+	ResMgr::fonts.pushPath(fs::current_path()/".."/"rsrc"/"fonts");
+	ResMgr::graphics.pushPath(fs::current_path()/".."/"rsrc"/"graphics");
+	fs::path dialogsPath = fs::current_path()/".."/"rsrc"/"dialogs";
 	
-	while (it != end) {
+	for(fs::directory_iterator it{dialogsPath}; it != fs::directory_iterator{}; ++it) {
 		fs::path path = it->path();
+		if(path.extension() != ".xml") continue;
 		std::string filename = path.stem().string();
 		CAPTURE(filename);
-		CHECK_NOTHROW({
-			DialogDefn* defn = load_dialog_defn(path);
-			cDialog dialog(*defn);
-			delete defn;
-		});
-		
-		++it;
+		CHECK_NOTHROW(load_dialog(path));
 	}
+} catch(...) {
+	ResMgr::fonts.popPath();
+	ResMgr::graphics.popPath();
+	throw;
 }
