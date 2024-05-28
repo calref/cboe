8a7976acacab38e6708001c9af9570933694436e
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-05-28 11:46:16 -0600

    enclose vcpkg git pull

--- .github/workflows/scripts/win/install-deps.bat
+++ .github/workflows/scripts/win/install-deps.bat
@@ -1,2 +1,2 @@
 
-cd C:\vcpkg && git pull && vcpkg update && vcpkg install zlib:%1-windows sfml:%1-windows opengl:%1-windows boost-any:%1-windows boost-dynamic-bitset:%1-windows boost-ptr-container:%1-windows boost-core:%1-windows boost-filesystem:%1-windows boost-system:%1-windows boost-date-time:%1-windows boost-chrono:%1-windows boost-math:%1-windows boost-spirit:%1-windows
+(cd C:\vcpkg && git pull) && vcpkg update && vcpkg install zlib:%1-windows sfml:%1-windows opengl:%1-windows boost-any:%1-windows boost-dynamic-bitset:%1-windows boost-ptr-container:%1-windows boost-core:%1-windows boost-filesystem:%1-windows boost-system:%1-windows boost-date-time:%1-windows boost-chrono:%1-windows boost-math:%1-windows boost-spirit:%1-windows
