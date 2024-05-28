72dfddc31e105dad6bfec5110d04d1158e5bef05
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-05-28 17:57:35 -0600

    Try different arg form and force scons to chain

--- .github/workflows/scripts/win/scons-build.bat
+++ .github/workflows/scripts/win/scons-build.bat
@@ -2,7 +2,5 @@
 setlocal enabledelayedexpansion
 
 for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -find **/Auxiliary/Build/vcvarsall.bat`) do (
-  %%i x86_amd64 "" 8.1
+  %%i x86_amd64 8.1 && scons
 )
-
-scons
