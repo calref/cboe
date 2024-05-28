682eb270ea2302e2a35de4f8f9cd8920f41be536
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-05-27 20:03:56 -0600

    linux build tgui without c++17 features

--- .github/workflows/scripts/linux/install-tgui.sh
+++ .github/workflows/scripts/linux/install-tgui.sh
@@ -3,7 +3,7 @@
 git clone --depth 1 -b 0.9 https://github.com/texus/TGUI.git
 cd TGUI
 export CLICOLOR_FORCE=1
-cmake .
+cmake -D CMAKE_CXX_STANDARD=14 .
 make
 cmake --install .
 cd .. # Probably not needed but...
