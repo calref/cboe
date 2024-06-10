089822ec8892fa7b1551590f0797c3d166bec15d
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-06-10 11:21:49 -0600

    debug more

--- .github/workflows/scripts/win/scons-build.bat
+++ .github/workflows/scripts/win/scons-build.bat
@@ -2,6 +2,7 @@
 setlocal enabledelayedexpansion
 
 for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -find **/Auxiliary/Build/vcvarsall.bat`) do (
+  @echo "%%i"
   call "%%i" x86_amd64
 )
 
