c2bffbe16798c51c9be7f0161743fcf3c0760b18
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-07-03 22:15:53 -0600

    parse std::map of replay action info elements

--- src/tools/replay.hpp
+++ src/tools/replay.hpp
@@ -20,6 +20,7 @@ extern void record_action(std::string action_type, std::map<std::string,std::str
 extern bool has_next_action();
 extern std::string next_action_type();
 extern Element* pop_next_action(std::string expected_action_type="");
+extern std::map<std::string,std::string> info_from_action(Element* action);
 extern std::string encode_file(fs::path file);
 extern void decode_file(std::string data, fs::path file);
 
