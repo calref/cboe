04b6b440b3ded9e0ae7370dc3975b3c8de14a102
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-07-03 22:15:25 -0600

    allow peeking the type of next replay action

--- src/tools/replay.hpp
+++ src/tools/replay.hpp
@@ -18,6 +18,7 @@ extern bool init_action_log(std::string command, std::string file);
 extern void record_action(std::string action_type, std::string inner_text);
 extern void record_action(std::string action_type, std::map<std::string,std::string> info);
 extern bool has_next_action();
+extern std::string next_action_type();
 extern Element* pop_next_action(std::string expected_action_type="");
 extern std::string encode_file(fs::path file);
 extern void decode_file(std::string data, fs::path file);
