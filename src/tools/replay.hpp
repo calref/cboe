64a9854c8a9d9254bc7023fce26babbe50d8b107
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-07-03 21:44:49 -0600

    allow recording action with multiple parameters

--- src/tools/replay.hpp
+++ src/tools/replay.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <sstream>
 #include <memory>
+#include <map>
 #include <boost/filesystem.hpp>
 
 // Input recording system
@@ -15,6 +16,7 @@ extern bool replaying;
 
 extern bool init_action_log(std::string command, std::string file);
 extern void record_action(std::string action_type, std::string inner_text);
+extern void record_action(std::string action_type, std::map<std::string,std::string> info);
 extern bool has_next_action();
 extern Element* pop_next_action(std::string expected_action_type="");
 extern std::string encode_file(fs::path file);
