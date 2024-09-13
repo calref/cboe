f9b3de763b4dc64e699d09b600bec79fe5609e07
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-09-13 18:18:10 -0500

    note duplicated gitrev build process

--- pkg/gitrev.sh
+++ pkg/gitrev.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Usage: gitrev.sh path-to-src
 
+# This script generates the header with git revision information
+# NOTE: Changes made here must also be made in SConstruct!
 REVID=`git rev-parse HEAD`;
 export FULLTAG=`git tag --sort=v:refname | tail -n1`;
 TAGREV=`git rev-parse $FULLTAG`;
