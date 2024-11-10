5c83fad1f9ef2aa408e8a5cd31c82beaa20bba28
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-11-10 14:46:35 -0600

    Generate about-boe.xml dynamically

--- pkg/generate-credits.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Call this manually every time DONORS.md or pkg/credits/credits.txt 
-# changes, and commit the generated files.
-
-# Requires:
-# - ImageMagick
-# - Rollout font
-
-# Usage: generate-credits.sh
