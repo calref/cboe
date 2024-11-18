9351d74f972fe74fdcb83b53850b92f860c9dd2f
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-11-18 08:29:42 -0600

    Re-add the original startanim graphic for future reference

--- pkg/generate-startanim.sh
+++ pkg/generate-startanim.sh
@@ -12,6 +12,6 @@ height=$((actual_line_size * line_count))
 magick -size 280x$height xc:transparent \
     -stroke black -pointsize $font_size -interline-spacing $spacing \
     -font 'pkg/credits/font-8.bdf' -annotate +7+$first_line_offset "$text" \
-    pkg/credits/startanim2.png
+    pkg/credits/startanim-pt-2.png
 
-magick convert -append pkg/credits/startanim*.png rsrc/graphics/startanim.png
+magick convert -append pkg/credits/startanim-pt-*.png rsrc/graphics/startanim.png
