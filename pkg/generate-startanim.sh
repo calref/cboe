af810e05c08f945c0164bb659e22493b87595448
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-11-17 20:22:13 -0600

    Make bitmapfont for startanim

--- pkg/generate-startanim.sh
+++ pkg/generate-startanim.sh
@@ -4,16 +4,14 @@ text="$(cat)"
 line_count=$(echo "$text" | wc -l)
 
 font_size=8
-spacing=0
-actual_line_size=$((font_size + spacing + 7))
+spacing=5
+actual_line_size=$((font_size + spacing))
 first_line_offset=$((font_size + 2))
 height=$((actual_line_size * line_count))
 
-# https://www.dafont.com/pix-chicago.font
-
 magick -size 280x$height xc:transparent \
     -stroke black -pointsize $font_size -interline-spacing $spacing \
-    -kerning 1 -font 'pkg/credits/pixChicago-edited.ttf' -annotate +6+$first_line_offset "$text" \
+    -font 'pkg/credits/font-8.bdf' -annotate +7+$first_line_offset "$text" \
     pkg/credits/startanim2.png
 
 magick convert -append pkg/credits/startanim*.png rsrc/graphics/startanim.png
