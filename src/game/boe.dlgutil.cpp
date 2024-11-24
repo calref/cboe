5b4cef4bc7a29cd95bff35bce73f520946f5a314
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-11-24 17:10:40 -0600

    fix event return value

--- src/game/boe.dlgutil.cpp
+++ src/game/boe.dlgutil.cpp
@@ -1244,6 +1244,7 @@ static bool prefs_event_filter (cDialog& me, std::string id, eKeyMod) {
 static bool reset_help(cDialog& me, std::string id, eKeyMod) {
 	if(cChoiceDlog("confirm-reset-help", {"yes", "no"}, &me).show() == "yes")
 		clear_pref("ReceivedHelp");
+	return true;
 }
 
 void pick_preferences(bool record) {
