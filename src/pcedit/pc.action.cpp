34785c3254bde74abef0b3e3dfa9aaa188994a40
Nat Quayle Nelson <natquaylenelson@gmail.com>
2025-01-16 16:49:33 -0600

    fix check_for_interrupt stubs in editors

--- src/pcedit/pc.action.cpp
+++ src/pcedit/pc.action.cpp
@@ -29,7 +29,7 @@ extern rectangle name_rect;
 extern rectangle pc_race_rect;
 extern rectangle edit_rect[5];
 
-bool check_for_interrupt(std::string) {}
+bool check_for_interrupt(std::string) { return false; }
 
 bool handle_action(const sf::Event & event) {
 	location the_point = translate_mouse_coordinates({event.mouseButton.x, event.mouseButton.y});
