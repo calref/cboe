2c3d4b08af2f8f612d8cae385289572bba1cfd5a
Nat Quayle Nelson <natquaylenelson@gmail.com>
2025-01-21 17:40:53 -0600

    fix shop and talk clicks falling through. Fix #544

--- src/game/boe.dlgutil.hpp
+++ src/game/boe.dlgutil.hpp
@@ -7,14 +7,14 @@
 
 bool start_shop_mode(short which,short cost_adj,std::string store_name, bool cancel_when_empty = false);
 void end_shop_mode();
-void handle_shop_event(location p, cFramerateLimiter& fps_limiter);
+bool handle_shop_event(location p, cFramerateLimiter& fps_limiter);
 void handle_sale(int i);
 void handle_info_request(int what_picked);
 void set_up_shop_array();
 void start_talk_mode(short m_num,short personality,mon_num_t monst_type,short store_face_pic);
 void end_talk_mode();
 void handle_talk_node(int which_talk_entry);
-void handle_talk_event(location p, cFramerateLimiter& fps_limiter);
+bool handle_talk_event(location p, cFramerateLimiter& fps_limiter);
 void handle_talk_spec(short ttype,char* place_string1,char* place_string2);
 void store_responses();
 void do_sign(short town_num, short which_sign, short sign_type);
