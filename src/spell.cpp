b14a238ee3374a5c31db4df5e2075171aac69794
Nat Quayle Nelson <natquaylenelson@gmail.com>
2025-01-21 12:40:19 -0600

    Don't allow Major Blessing in town mode. Fix #541
    It seemed reasonable that we actually would want to LET
    the player cast this in town mode, but it seems like other
    blessing spells (i.e. Haste) do not

--- src/spell.cpp
+++ src/spell.cpp
@@ -212,7 +212,7 @@ cSpell M_FLIGHT = cSpell(eSpell::FLIGHT).asType(eSkill::MAGE_SPELLS).asLevel(6)
 cSpell M_SHOCKWAVE = cSpell(eSpell::SHOCKWAVE).asType(eSkill::MAGE_SPELLS).asLevel(7)
 	.withCost(12).withRefer(REFER_IMMED).when(WHEN_COMBAT).finish();
 cSpell M_BLESS_MAJOR = cSpell(eSpell::BLESS_MAJOR).asType(eSkill::MAGE_SPELLS).asLevel(7)
-	.withCost(8).withRefer(REFER_IMMED).when(WHEN_COMBAT).when(WHEN_TOWN).asPeaceful().finish();
+	.withCost(8).withRefer(REFER_IMMED).when(WHEN_COMBAT).asPeaceful().finish();
 cSpell M_PARALYSIS_MASS = cSpell(eSpell::PARALYSIS_MASS).asType(eSkill::MAGE_SPELLS).asLevel(7)
 	.withRange(8).withCost(20).withRefer(REFER_IMMED).when(WHEN_COMBAT).finish();
 cSpell M_PROTECTION = cSpell(eSpell::PROTECTION).asType(eSkill::MAGE_SPELLS).asLevel(7)
