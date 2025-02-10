4a911f1cb20846ba6d7ec1d31abc6d4598ec396c
Nat Quayle Nelson <natquaylenelson@gmail.com>
2025-02-10 15:55:31 -0600

    Debug action: kill the party

--- src/game/boe.main.cpp
+++ src/game/boe.main.cpp
@@ -771,6 +771,9 @@ static void replay_action(Element& action) {
 	}else if(t == "debug_kill"){
 		debug_kill();
 		return;
+	}else if(t == "debug_kill_party"){
+		debug_kill_party();
+		return;
 	}else if(t == "debug_magic_map"){
 		debug_magic_map();
 		return;
