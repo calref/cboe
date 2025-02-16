89dbe98b2bd1fc72102cbdb6c4522a89371ba66a
Celtic Minstrel <celtic.minstrel.ca@some.place>
2025-02-16 13:16:42 -0500

    Refactor special node properties struct to isolate the magic characters.
    Basically what that means is that the magic characters are only parsed at startup – the scenario editor code doesn't need to understand them, instead using an enum.
    
    This is part of the work for #607

--- src/scenedit/scen.keydlgs.hpp
+++ src/scenedit/scen.keydlgs.hpp
@@ -4,18 +4,6 @@
 #include "location.hpp" // for info_rect_t
 class cDialog;
 
-enum eStrType {
-	STRT_MONST, STRT_ITEM, STRT_TER, STRT_BUTTON,
-	STRT_SPEC_ITEM, STRT_MAGE, STRT_PRIEST, STRT_ALCHEMY,
-	STRT_TOWN, STRT_SECTOR, STRT_SKILL, STRT_TRAIT, STRT_RACE,
-	STRT_PICT, STRT_CMP, STRT_ACCUM, STRT_TRAP,
-	STRT_ATTITUDE, STRT_STAIR, STRT_LIGHT, STRT_CONTEXT,
-	STRT_SHOP, STRT_COST_ADJ, STRT_STAIR_MODE, STRT_TALK_NODE,
-	STRT_STATUS, STRT_SPELL_PAT, STRT_SUMMON, STRT_TALK, STRT_TALK_NODE_PERSON,
-	STRT_ENCHANT, STRT_DIR, STRT_QUEST, STRT_QUEST_STATUS,
-	STRT_HEALING, STRT_TREASURE, STRT_MONST_STAT, STRT_POS_MODE,
-};
-
 bool cre(short val,short min,short max,std::string text1,std::string text2,cDialog* parent) ;
 void display_strings(char *text1, char *text2,
 	char *title,short sound_num,short graphic_num,short graphic_type,cDialog* parent);
