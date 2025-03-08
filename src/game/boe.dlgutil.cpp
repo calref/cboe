8a1cae85aee81ca556f92e7978385aa263dd2a59
Nat Quayle Nelson <natquaylenelson@gmail.com>
2025-03-08 09:50:52 -0600

    watch cursor when scenario picker is building headers

--- src/game/boe.dlgutil.cpp
+++ src/game/boe.dlgutil.cpp
@@ -1792,7 +1792,11 @@ public:
 };
 
 scen_header_type pick_a_scen() {
-	return cChooseScenario().run();
+	// build_scen_headers() can be slow.
+	set_cursor(watch_curs);
+	cChooseScenario dlg;
+	restore_cursor();
+	return dlg.run();
 }
 
 extern fs::path saveDir;
