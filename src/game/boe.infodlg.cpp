d17715f71feaacb9fb11ec6e790d689d81ed2512
Nat Quayle Nelson <natquaylenelson@gmail.com>
2025-08-06 15:17:56 -0500

    combine 2-string dialog records into 1 element

--- src/game/boe.infodlg.cpp
+++ src/game/boe.infodlg.cpp
@@ -707,10 +707,10 @@ void cStringRecorder::operator()(cDialog& me) {
 	play_sound(0);
 	std::string str1, str2;
 	univ.get_strs(str1, str2, spec_type, label1, label2);
-	std::string combined = str1;
+	// Combine str1 and str2 in one journal entry:
 	if(!str2.empty())
-		combined += " ||" + str2;
-	if(univ.party.record(note_type, combined, location)){
+		str1 += " ||" + str2;
+	if(univ.party.record(note_type, str1, location)){
 		give_help(58,0,me);
 		ASB("Added to encounter notes.");
 	}
